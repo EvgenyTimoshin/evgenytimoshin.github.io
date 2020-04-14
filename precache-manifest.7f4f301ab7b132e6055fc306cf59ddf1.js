09b18b71beab83b10da1b595841fc8774e1f38b8
evgenytimoshin <evgenytimoshin@gmail.com>
2020-04-15 01:59:53 +0200

    Updates

---
+++ precache-manifest.7f4f301ab7b132e6055fc306cf59ddf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c5167c6be4246861952c21dce62909",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d96530426941c9b2d9fb",
+    "url": "/portfolio/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "e8eda0f47cdc20b8c411",
+    "url": "/portfolio/static/css/main.ac5893af.chunk.css"
+  },
+  {
+    "revision": "d96530426941c9b2d9fb",
+    "url": "/portfolio/static/js/2.e4ed15cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.e4ed15cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8eda0f47cdc20b8c411",
+    "url": "/portfolio/static/js/main.13db919f.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
